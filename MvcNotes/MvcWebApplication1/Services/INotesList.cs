edfee3715fbbb9d45ad28dd6bd731843d0226631
shoxlaa <shahla.guliyeva02@gmail.com>
2022-11-21 18:24:50 +0400

    Added Mvc Project for test

---
+++ MvcNotes/MvcWebApplication1/Services/INotesList.cs
@@ -0,0 +1,32 @@
+﻿using MvcWebApplication1.Models;
+namespace MvcWebApplication1.Services
+{
+    public interface INotesList
+    {
+        void AddItem(Note item);
+        void RemoveItem(int index);
+        IEnumerable<Note> GetItems();
+    }
+    public class NotesList : INotesList
+    {
+		private List<Note> _items = new();
+
+		public void AddItem(Note item)
+		{
+			_items.Add(item);
+		}
+
+		public IEnumerable<Note> GetItems()
+		{
+			for (int i = 0; i < _items.Count; i++)
+			{
+				yield return _items[i];
+			}
+		}
+
+		public void RemoveItem(int index)
+		{
+			_items.RemoveAt(index);
+		}
+	}
+}
