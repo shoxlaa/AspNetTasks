4f577c30a32c7a3d54707da1a38f44454ff5bb17
Guli_tp48 <shahla.guliyeva02@gmail.com>
2022-12-08 21:49:28 +0400

    it doesnt work

---
+++ WebApiMystat/Controllers/Version1/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiMystat.Models;
+using WebApiMystat.Serveces;
+
+namespace WebApiMystat.Controllers.Version1;
+
+[ApiController]
+[Route("api/v1/{controller}")]
+public class UserController : Controller
+{
+    private readonly MyStatDbContext _myStatDbContext;
+    
+    [HttpGet("get/{id:int}")]
+    public async Task<User?> Get(int id)
+    {
+        return await _myStatDbContext.Users.SingleOrDefaultAsync(u => u.Id == id);
+    }
+    [HttpGet("get")]
+    public async Task<IEnumerable<User>> Get()
+    {
+        return await _myStatDbContext.Users.ToListAsync();
+    }
+
+    [HttpPost("Add/User")]
+    public async Task<HttpResponseMessage> Add(User? user)
+    {
+        if (user == null)
+        {
+            return new HttpResponseMessage()
+            {
+                StatusCode = System.Net.HttpStatusCode.BadRequest,
+            };
+        }
+
+        await _myStatDbContext.Users.AddAsync(user);
+
+        return new HttpResponseMessage()
+        {
+            StatusCode = System.Net.HttpStatusCode.OK
+        };
+    }
+}
---
+++ WebApiMystat/Serveces/MyStatDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using WebApiMystat.Models;
+
+namespace WebApiMystat.Serveces; 
+
+
+public class MyStatDbContext :DbContext
+{
+    public  MyStatDbContext(DbContextOptions<MyStatDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<HomeWork> HomeWorks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        int id = 1;
+        var faker = new Faker<User>();
+        var users = faker.RuleFor(u => u.Name, f => f.Person.FirstName)
+            .RuleFor(u => u.Surname, f => f.Person.LastName)
+            .RuleFor(u => u.Password, f => f.Random.Words(2))
+            .Generate(10);
+        
+        var fakerHw = new Faker<HomeWork>();
+        var hw = fakerHw.RuleFor(h => h.Title, f => f.Hacker.Noun())
+            .RuleFor(h => h.File, f => f.Hacker.Verb())
+            .Generate(10);
+
+        modelBuilder.Entity<User>().HasData(users);
+        modelBuilder.Entity<HomeWork>().HasData(hw);
+    }
+}
